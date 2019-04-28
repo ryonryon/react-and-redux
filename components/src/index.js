fc6f8ac70bda79de9627367406a8fc9d22b56819
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-27 18:34:49 -0700

    made avatar into variable.

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sum" />
-      <CommentDetail author="Ryo" />
-      <CommentDetail author="Kim" />
+      <CommentDetail
+        author="Sum"
+        timeAgo="Today at 4:45PM"
+        content="Nice blog!!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Ryo"
+        timeAgo="Today at 11:45AM"
+        content="Soso blog!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Kim"
+        timeAgo="Yesterday at 6:13AM"
+        content="Looks nice!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
